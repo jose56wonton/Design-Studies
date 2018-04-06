f03a4ee27c573ae38120d39e5cc27124376105cb
Joshua Wootonn <joshuawootonn@gmail.com>
2018-04-06 16:28:54 -0500

    Got the new font and images all stuck in there nice and clean liek

--- project-slideshow/src/App.js
+++ project-slideshow/src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from "react";
 import "./styles/styles.css";
+
+const data = [
+  {
+    title: "mhint",
+    subTitle: "Your life smarter",
+    what: "Direction, User Experience, User Interface, Development and Advertising."
+  },
+  {
+    title: "plac",
+    subTitle: "Make every day special",
+    what: "Research, Development and Distribution."
+  },
+  {
+    title: "kira",
+    subTitle: "The most powerful tool for social analytics.",
+    what: "Research,Studio and Design."
+  },
+  {
+    title: "yode",
+    subTitle: "Feeling good.",
+    what: "Research, User Experience, User Interface and Development."
+  }
+]
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      current: 0,
+      data: data
+    }
+  }
+  
   render() {
+    var styles = {
+      backgroundImage: `${this.state.current}")`
+  }
     return (
       <div className="page">
         <div className="container">
           <div className="largeCard">
             <div className="left">
-            <h1>Mhint</h1>
+              <h1>{this.state.data[0].title}</h1>
             </div>
-            <div className="right">
-            
-            </div>
-            
+            <div className={`right background-${this.state.current}`}/>
           </div>
           <div className="smallCard">
             <div className="textTop">
