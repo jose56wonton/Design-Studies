7f1fd937c6e30a33285fb1f1928561dce24863d6
Joshua Wootonn <joshuawootonn@gmail.com>
2018-04-06 18:58:50 -0500

    Added images and added functional switching between slides

--- project-slideshow/src/App.js
+++ project-slideshow/src/App.js
@@ -3,63 +3,76 @@ import "./styles/styles.css";
 
 const data = [
   {
-    title: "mhint",
+    title: "Mhint",
     subTitle: "Your life smarter",
-    what: "Direction, User Experience, User Interface, Development and Advertising."
+    what:
+      "Direction, User Experience, User Interface, Development and Advertising."
   },
   {
-    title: "plac",
+    title: "Plac",
     subTitle: "Make every day special",
     what: "Research, Development and Distribution."
   },
   {
-    title: "kira",
+    title: "Kira",
     subTitle: "The most powerful tool for social analytics.",
     what: "Research,Studio and Design."
   },
   {
-    title: "yode",
+    title: "Yode",
     subTitle: "Feeling good.",
     what: "Research, User Experience, User Interface and Development."
   }
-]
-
+];
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       current: 0,
       data: data
+    };
+  }
+  left = () => {
+    if (this.state.current !== 0) {
+      this.setState({
+        current: this.state.current - 1
+      });
     }
-  }
-  
+  };
+  right = () => {
+    if (this.state.current !== this.state.data.length-1) {
+      this.setState({
+        current: this.state.current + 1
+      });
+    }
+  };
   render() {
-    var styles = {
-      backgroundImage: `${this.state.current}")`
-  }
+    console.log(this.state.current);
+    const currentData = this.state.data[this.state.current];
     return (
       <div className="page">
         <div className="container">
           <div className="largeCard">
             <div className="left">
-              <h1>{this.state.data[0].title}</h1>
+              <h1>{currentData.title}</h1>
             </div>
-            <div className={`right background-${this.state.current}`}/>
+            <div className={`right background-${this.state.current}`} />
           </div>
           <div className="smallCard">
             <div className="textTop">
-              <h2>mhint</h2>
-              <h4>Your life smarter.</h4>
+              <h2>{currentData.title}</h2>
+              <h4>{currentData.sub}</h4>
             </div>
             <div className="textBottom">
               <p>
                 <span>What we did</span>
                 <br />
-                Direction, User Experience, User Interface, Development and
-                Advertising.
+                {currentData.what}
               </p>
             </div>
+            <a onClick={this.left}>Left</a>
+            <a onClick={this.right}>Right</a>
           </div>
         </div>
       </div>
